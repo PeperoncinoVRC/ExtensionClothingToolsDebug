1685d4f09ba6f2bd1915c6cf4f56d422cdcda1eb
Reimhak <peperoncino.vrc@gmail.com>
2024-06-14 23:28:15 +0900

    First Tools

---
+++ Assets/EXTENSION CLOTHING/EXTENSION CLOTHING Tools/ECTool_1.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ECTool_1 : EditorWindow
+{
+    // ウィンドウの内容を描画
+    private void OnGUI()
+    {
+        GUILayout.Label("This is the ECTool_1 window.", EditorStyles.boldLabel);
+    }
+}
---
+++ Assets/EXTENSION CLOTHING/EXTENSION CLOTHING Tools/ExtensionClothingTools.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ExtensionClothingTools : EditorWindow
+{
+    // ウィンドウを表示するためのメニュー項目を追加
+    [MenuItem("EXTENSION CLOTHING/ECTool_1")]
+    public static void ShowECTool1Window()
+    {
+        // ウィンドウを作成して表示
+        GetWindow<ECTool_1>("ECTool_1 Window");
+    }
+
+    // 他のツールウィンドウも同様に追加可能
+    // [MenuItem("EXTENSION CLOTHING/AnotherTool")]
+    // public static void ShowAnotherToolWindow()
+    // {
+    //     GetWindow<AnotherTool>("Another Tool Window");
+    // }
+}
